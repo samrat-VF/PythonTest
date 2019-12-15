31bae5c7b2096430a378b0c5eb7f271a587987bc
samrat-VF <samrat.nitrkl@gmail.com>
2019-12-15 11:51:26 +1100

    Initial Commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+import requests
+
+print(sys.executable)
+print(sys.version)
+
+
+def greetings(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://www.google.com")
+print(r.status_code)
