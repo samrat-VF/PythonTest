495d13dd50a64e3905fb936f017cbf3147fd5b96
samrat-VF <samrat.som@voicefoundry.com.au>
2019-12-15 13:02:05 +1100

    Hello

--- JustTrack.py
+++ JustTrack.py
@@ -2,4 +2,4 @@ import sys
 
 print(sys.executable)
 print(sys.version)
-print("Removeas the Change to check Rebase Functionality")
+print("Removeas the Change to check Rebase Functionality after the config")
